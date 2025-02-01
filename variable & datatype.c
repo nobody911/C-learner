949f38d34ffeadcb2c040fc1d0cbc6b5ea96fc8d
nobody911 <kroxgod@LAPTOP-3E5FQLAH.>
2025-02-02 02:18:47 +0530

    variable and datatype in c

---
+++ variable & datatype.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int age = 19;
+    float cgpa = 4.5;
+    char grade = 'C';
+    char name[] = "Dude";
+    printf("\nYour name is %s\n", name);
+    printf("You are %d years old\n", age);
+    printf("You have a cgpa of %.1f with a grade of %c", cgpa, grade);
+
+    // further datatypes in c are
+
+    
+    return 0;
+}
